64e0629be722a2c87536f3ffa7bf550d0256ded8
Pierre-Jean Baraud <pierre-jean@baraud.fr>
2024-10-20 23:54:32 +0100

    Add nvim config

---
+++ base/nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,21 @@
+return {
+  { 
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup(
+        {
+          flavour = "frappe"
+        }
+      )
+      vim.cmd.colorscheme "catppuccin"
+    end
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin"
+    }
+  }
+}
